3cfcfc2f3596eb39b7de389e3de6643b52cbc990
rui.li <rui.li@cloudminds.com>
2022-07-15 11:26:23 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#############################################
+# File Name: setup.py
+# Author: mage
+# Mail: mage@woodcol.com
+# Created Time:  2018-1-23 19:17:34
+#############################################
+from setuptools import setup, find_packages
+setup(
+    name="pyCeph",
+    version="0.1.0",
+    keywords=("pip", "pyCeph", "s3", "ceph"),
+    description="ceph using tool",
+    long_description="ceph using tool",
+    license="MIT Licence",
+    url="https://github.com/susufqx/pyCeph",
+    author="susufqx",
+    author_email="jiangsulirui@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
